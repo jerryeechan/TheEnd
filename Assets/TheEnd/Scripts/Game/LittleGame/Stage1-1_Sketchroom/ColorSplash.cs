ad4f12c5022c3854ef1073b9d7e59d37e6a4d814
jerrypupu111 <pupu1416@yahoo.com.tw>
2016-01-05 16:39:43 +0800

    1/5

---
+++ Assets/TheEnd/Scripts/Game/LittleGame/Stage1-1_Sketchroom/ColorSplash.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ColorSplash : MonoBehaviour, IPointerEnterHandler
+{
+
+    Image image;
+	bool hasDrawn = false;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		if(hasDrawn == false)
+     	{
+			 drawColor();
+			 hasDrawn = true;
+		}
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        image.canvasRenderer.SetAlpha(0);
+    }
+    
+
+    void drawColor()
+    {
+        image.CrossFadeAlpha(1, 1, false);
+        PaintingGame.instance.splashDrawn();
+    }
+
+}
---
+++ Assets/TheEnd/Scripts/Game/LittleGame/Stage1-1_Sketchroom/PaintingGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PaintingGame : Singleton<PaintingGame> {
+
+	ColorSplash[] splashes;
+	int drawnNum;
+	
+	void Awake()
+	{
+		splashes = GetComponentsInChildren<ColorSplash>();
+	}
+	public void splashDrawn()
+	{
+		print("drawn");
+		drawnNum++;
+		if(drawnNum == splashes.Length)
+		{
+			gameDone();	
+		}
+	}
+	
+	void gameDone()
+	{
+		print("Done");
+	}
+}
